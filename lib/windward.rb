d4724b30ae153081f4bd2131a178942ed11d2e26
Yann VERY <yann.very@gmail.com>
2015-03-10 22:22:17 +0100

    Keep weather and remove region

--- lib/windward.rb
+++ lib/windward.rb
@@ -71,7 +71,7 @@ module Windward
         data = page.parser.css("div##{values['value']}")
         previsions = Hash.new
         data.each do |datum|
-          temps = datum.css('span.picTemps').first.content.strip
+          temps = datum.css('span.picTemps').first.content.split(' - ').last.strip
           temper = datum.css('span.temper').first.content.strip
           department = @departments[datum['data-insee'][0..1]]
           city = @cities[datum['data-insee'][0..4]]
