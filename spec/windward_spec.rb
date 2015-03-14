959b0b224a6d204e3fc2a69500bbda790818c55e
Yann VERY <yann.very@gmail.com>
2015-03-14 14:32:38 +0100

    Add some tests

--- spec/windward_spec.rb
+++ spec/windward_spec.rb
@@ -5,5 +5,25 @@ describe "Windward", :vcr => true do
     it "should get new windward weather" do
       expect(Windward::Weather.new).to_not be_nil
     end
+
+    it "should returns regions list" do
+      weather = Windward::Weather.new
+      expect(weather.regions).to include "Alsace"
+    end
+
+    it "should return a hash with slug value and previsions of region" do
+      weather = Windward::Weather.new
+      expect(weather.region "Alsace").to  match a_hash_including("slug", "value", "previsions")
+    end
+
+    it "should return a hash that contains a previsions hash of region" do
+      weather = Windward::Weather.new
+      expect(weather.region("Alsace")["previsions"].values[0]).to  match a_hash_including("temps", "temper", "city")
+    end
+
+    it "should return only previsions hash of region" do
+      weather = Windward::Weather.new
+      expect(weather.previsions("Alsace").values[0]).to  match a_hash_including("temps", "temper", "city")
+    end
   end
 end
