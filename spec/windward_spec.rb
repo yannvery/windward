7a4df1da5e76713a72aa85a6863d4ec4436257e8
Yann VERY <yann.very@gmail.com>
2015-03-14 14:07:12 +0100

    Update tests

--- spec/windward_spec.rb
+++ spec/windward_spec.rb
@@ -1,9 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Windward" do
+describe "Windward", :vcr => true do
   describe "Weather" do
     it "should get new windward weather" do
-      Windward::Weather.new.should_not be_nil
+      expect(Windward::Weather.new).to_not be_nil
     end
   end
 end
