798275e8378710eacdccde63498ff7df22cbadca
Yann VERY <yann.very@gmail.com>
2015-03-15 14:57:58 +0100

    Use quote instead of double quote

--- spec/windward_spec.rb
+++ spec/windward_spec.rb
@@ -1,29 +1,29 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Windward", vcr: true do
-  describe "Weather" do
-    it "should get new windward weather" do
+describe 'Windward', vcr: true do
+  describe 'Weather' do
+    it 'should get new windward weather' do
       expect(Windward::Weather.new).to_not be_nil
     end
 
-    it "should returns regions list" do
+    it 'should returns regions list' do
       weather = Windward::Weather.new
-      expect(weather.regions).to include "Alsace"
+      expect(weather.regions).to include 'Alsace'
     end
 
-    it "should return a hash with slug value and previsions of region" do
+    it 'should return a hash with slug value and previsions of region' do
       weather = Windward::Weather.new
-      expect(weather.region "Alsace").to  match a_hash_including("slug", "value", "previsions")
+      expect(weather.region 'Alsace').to  match a_hash_including('slug', 'value', 'previsions')
     end
 
-    it "should return a hash that contains a previsions hash of region" do
+    it 'should return a hash that contains a previsions hash of region' do
       weather = Windward::Weather.new
-      expect(weather.region("Alsace")["previsions"].values[0]).to  match a_hash_including("temps", "temper", "city")
+      expect(weather.region('Alsace')['previsions'].values[0]).to  match a_hash_including('temps', 'temper', 'city')
     end
 
-    it "should return only previsions hash of region" do
+    it 'should return only previsions hash of region' do
       weather = Windward::Weather.new
-      expect(weather.previsions("Alsace").values[0]).to  match a_hash_including("temps", "temper", "city")
+      expect(weather.previsions('Alsace').values[0]).to  match a_hash_including('temps', 'temper', 'city')
     end
   end
 end
