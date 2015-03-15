106b83dcd80a1ae4742676715600f3c90183e0db
Yann VERY <yann.very@gmail.com>
2015-03-15 14:53:38 +0100

    Fix hash syntax

--- spec/windward_spec.rb
+++ spec/windward_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Windward", :vcr => true do
+describe "Windward", vcr: true do
   describe "Weather" do
     it "should get new windward weather" do
       expect(Windward::Weather.new).to_not be_nil
