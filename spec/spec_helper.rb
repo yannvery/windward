ae21a876ae62a87183ca4d2494ee3778044319f0
Yann VERY <yann.very@gmail.com>
2015-03-15 14:51:15 +0100

    Add space between { and |

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ WebMock.disable_net_connect!(allow_localhost: true)
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   config.before(:each) do
