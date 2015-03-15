e4e3d0bface2ddc24cccf8b0b8880dd888c2417a
Yann VERY <yann.very@gmail.com>
2015-03-15 14:50:11 +0100

    Remove unused lines and fix tab

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,9 +20,7 @@ end
 require 'vcr'
 
 VCR.configure do |c|
-    c.hook_into :webmock
-    c.cassette_library_dir = 'spec/support/vcr_cassettes'
-    c.configure_rspec_metadata!
-    #c.allow_http_connections_when_no_cassette = true if real_requests
-    #c.default_cassette_options = {:record => :new_episodes}
+  c.hook_into :webmock
+  c.cassette_library_dir = 'spec/support/vcr_cassettes'
+  c.configure_rspec_metadata!
 end
